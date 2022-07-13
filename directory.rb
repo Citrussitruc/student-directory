c692dbc12d6ead868b494990a8c7cf7742c0bee7
Curtis Turk <curtis.c.turk@outlook.com>
2022-07-13 16:12:46 +0100

    added a group by cohort method

--- directory.rb
+++ directory.rb
@@ -2,14 +2,14 @@ students = [
   {name: "Dr. Hannibal Lecter", cohort: :november, from: :USA},
   {name: "Darth Vader", cohort: :november, from: :Tatooine},
   {name: "Nurse Ratched", cohort: :november, from: :USA},
-  {name: "Michael Corleone", cohort: :november, from: :USA},
-  {name: "Alex DeLarge", cohort: :november, from: :USA},
+  {name: "Michael Corleone", cohort: :november, from: :NY},
+  {name: "Alex DeLarge", cohort: :november, from: :England},
   {name: "The Wicked Witch of the West", cohort: :november, from: :The_West},
   {name: "Terminator", cohort: :november, from: :The_Future},
   {name: "Freddy Krueger", cohort: :november, from: :USA},
   {name: "The Joker", cohort: :november, from: :Gotham},
   {name: "Joffrey Baratheon", cohort: :november, from: :Westeros},
-  {name: "Norman Bates", cohort: :november, from: :USA}
+  {name: "Norman Bates", cohort: :december, from: :USA}
 ]
 
 def print_header
@@ -19,14 +19,14 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]}, #{student[:from]} (#{student[:cohort]} cohort)".center(200)
+    puts "#{index + 1}. #{student[:name]}, #{student[:from]} (#{student[:cohort]})".center(200)
   end
 end
 
 def while_print(students)
   i = 0
   while i < students.length
-    puts "#{i + 1}. #{students[i][:name]}, #{students[i][:from]} (#{students[i][:cohort]} cohort)".center(200)
+    puts "#{i + 1}. #{students[i][:name]}, #{students[i][:from]} (#{students[i][:cohort]})".center(200)
     i += 1
   end
 end
@@ -43,9 +43,9 @@ def input_students(students)
   puts "name"
   name = gets.chomp
   puts "cohort"
-  cohort = gets.chomp.to_s
+  cohort = gets.chomp.to_sym
   puts "from"
-  from = gets.chomp.to_s
+  from = gets.chomp.to_sym
   # while name is not empty, repeat this code
   while !name.empty? && !cohort.empty? do
     # Add student hash to array
@@ -54,9 +54,9 @@ def input_students(students)
     puts "name"
     name = gets.chomp
     puts "cohort"
-    cohort = gets.chomp.to_s
+    cohort = gets.chomp.to_sym
     puts "from"
-    from = gets.chomp.to_s
+    from = gets.chomp.to_sym
   end 
   # Return array
   students
@@ -72,14 +72,27 @@ def shorter_than_12_chars(students)
   students.select { |student| student[:name].length < 12 }.each { |student| puts student[:name] }
 end
 
-print_header
-print(students)
-print_footer(students)
+def group_by_cohort(students) 
+  puts "Which cohort would you like to view?"
+  cohort = gets.chomp
+  
+  students.each do |student|
+    if student[:cohort] == cohort.to_sym
+      puts student[:name]
+    end
+  end
+end
 
-# while_print(students)
+# print_header
+# print(students)
+# print_footer(students)
 
-print input_students(students)
+# # while_print(students)
+
+# print input_students(students)
 
 # specific_letter(students)
 
 # shorter_than_12_chars(students)
+
+group_by_cohort(students)
