0b863dfa3bd7d36b8d34ceb02aea630d1818e0bf
Curtis Turk <curtis.c.turk@outlook.com>
2022-07-13 10:52:21 +0100

    adding comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First print the list of students
 puts "The students of Villans Academy"
 puts "-------------------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,8 @@ puts "Freddy Kruger"
 puts "The Joker"
 puts "Joffery Baratheon"
 puts "Norman Bates"
-
+# Print total numbers
 print "Overall, we have "
 print 11
+# Print doesn't add new line comments
 puts " great students"
