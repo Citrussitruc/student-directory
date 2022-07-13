b07f75c2204b10c140161818f3a02d0914a63063
Curtis Turk <curtis.c.turk@outlook.com>
2022-07-13 15:45:07 +0100

    Made some changes and combined some stuff

--- directory.rb
+++ directory.rb
@@ -13,50 +13,53 @@ students = [
 ]
 
 def print_header
-  puts "The students of Villans Academy"
-  puts "-------------------------------"
+  puts "The students of Villans Academy".center(200)
+  puts "-------------------------------".center(200)
 end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]}, #{student[:from]} (#{student[:cohort]} cohort)"
+    puts "#{index + 1}. #{student[:name]}, #{student[:from]} (#{student[:cohort]} cohort)".center(200)
   end
 end
 
 def while_print(students)
   i = 0
   while i < students.length
-    puts "#{i + 1}. #{students[i][:name]}, #{students[i][:from]} (#{students[i][:cohort]} cohort)"
+    puts "#{i + 1}. #{students[i][:name]}, #{students[i][:from]} (#{students[i][:cohort]} cohort)".center(200)
     i += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "-------------------------------".center(200)
+  puts "Overall, we have #{students.count} great students".center(200)
 end
 
-def input_students
+def input_students(students)
   puts "Please enter the names of the student and then which cohort"
-  puts "To skip adding info, just press return"
-  # To create an empty array
-  new_cohort = []
+  puts "To skip adding info, just press return twice."
   # To get the first name
   puts "name"
   name = gets.chomp
   puts "cohort"
   cohort = gets.chomp.to_s
+  puts "from"
+  from = gets.chomp.to_s
   # while name is not empty, repeat this code
-  while !name.empty? do
+  while !name.empty? && !cohort.empty? do
     # Add student hash to array
-    new_cohort << {name: name, cohort: cohort}
+    students << {name: name, cohort: cohort, from: from}
     # Get another name
     puts "name"
     name = gets.chomp
     puts "cohort"
     cohort = gets.chomp.to_s
+    puts "from"
+    from = gets.chomp.to_s
   end 
   # Return array
-  new_cohort
+  students
 end
 
 def specific_letter(students)
@@ -69,15 +72,14 @@ def shorter_than_12_chars(students)
   students.select { |student| student[:name].length < 12 }.each { |student| puts student[:name] }
 end
 
-# print_header
+print_header
 print(students)
-# print_footer(students)
+print_footer(students)
 
-while_print(students)
+# while_print(students)
 
-# new_cohort = input_students
-# print(new_cohort)
-
+print input_students(students)
 
 # specific_letter(students)
+
 # shorter_than_12_chars(students)
